039c05f8c472fc3930cc2fbad24c9fd0a444596d
Thort <yamaxanado@gmail.com>
2017-09-11 10:53:03 +0000

    fix:heroku migration error

--- db/migrate/20170910093940_change_trackid_to_apps.rb
+++ db/migrate/20170910093940_change_trackid_to_apps.rb
@@ -1,7 +1,7 @@
 class ChangeTrackidToApps < ActiveRecord::Migration[5.0]
   #変更後の型
   def up
-    change_column :apps, :trackid, :integer, null: false
+    change_column :apps, :trackid, 'integer USING CAST(column_name AS integer)', null: false
   end
 
   #変更前の型
